6392f69bd266387eebd2c97f3914bd4e4bc9539d
Chichmarin17 <pits1917burg@gmail.com>
2022-03-18 11:21:13 +0300

    feat: add lint script for mobile

--- apps/mobile/src/app/App.spec.tsx
+++ apps/mobile/src/app/App.spec.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { render } from '@testing-library/react-native';
 
-import App from './App';
+import { App } from './App';
 
 test('renders correctly', () => {
   const { getByTestId } = render(<App />);
