3057b30874d4504e0739395456f915e6d2a50d61
Chichmarin17 <pits1917burg@gmail.com>
2022-03-24 20:56:39 +0300

    feat: add global .env

--- packages/apiserver/src/app/app.module.ts
+++ packages/apiserver/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' })],
   controllers: [AppController],
   providers: [AppService],
 })
