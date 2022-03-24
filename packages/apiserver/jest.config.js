76c2ec29f2f7c5e060bfe44cb7785507b0a81e11
Ivan <82332668+IvanUglovets@users.noreply.github.com>
2022-03-24 15:38:38 +0300

    add NestJs app (#11)
    Co-authored-by: Ivan Uglovets <uglovec@gmil.com>

--- packages/apiserver/jest.config.js
+++ packages/apiserver/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'apisrv',
+  displayName: 'apiserver',
   preset: '../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -11,5 +11,5 @@ module.exports = {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/packages/apisrv',
+  coverageDirectory: '../../coverage/packages/apiserver',
 };
