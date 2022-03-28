ad4e6471cd6ff46a4dfe61ca35fb04ceedfd3e3e
Chichmarin17 <pits1917burg@gmail.com>
2022-03-28 13:15:30 +0300

    feat: create initial migrations

--- packages/apiserver/src/app/app.module.ts
+++ packages/apiserver/src/app/app.module.ts
@@ -11,12 +11,17 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
 import { AppResolver } from './app.resolver';
+import {PersonEntity} from "../resources/persons/person.entity";
+import {MaterialEntity} from "../resources/materials/material.entity";
+import {StatusEntity} from "../resources/statuses/status.entity";
 
 @Module({
   imports: [
       ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
       TypeOrmModule.forRoot({
         ...typeorm,
+        entities: [PersonEntity, MaterialEntity, StatusEntity],
+        migrations: [`${__dirname}/packages/apiserver/src/migrations/*.ts`],
       }),
       MaterialModule,
       StatusModule,
