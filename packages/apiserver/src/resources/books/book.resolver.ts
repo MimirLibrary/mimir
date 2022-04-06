593bea58db16c9161ff5aaaed215f4c0b7dfec5d
Ivan Uglovets <uglovec@gmil.com>
2022-04-06 15:28:47 +0300

    fix code

--- packages/apiserver/src/resources/books/book.resolver.ts
+++ packages/apiserver/src/resources/books/book.resolver.ts
@@ -10,22 +10,26 @@ export class BookResolver {
     @Args('person_id') person_id: number,
     @Args('identifier') identifier: string
   ) {
-    const material = await Material.findOne({ where: { identifier } });
-    if (!material) {
-      throw new BadRequestException(
-        'This book is not registered in the library'
-      );
+    try {
+      const material = await Material.findOne({ where: { identifier } });
+      if (!material) {
+        return new BadRequestException(
+          'This book is not registered in the library'
+        );
+      }
+      const { id } = material;
+      const statuses = await Status.find({
+        where: { material_id: id, person_id },
+      });
+      const lastStatus = statuses[statuses.length - 1];
+      if (lastStatus.status === 'Busy') {
+        return new BadRequestException(
+          `This book is busy or doesn't exist. Ask the manager.`
+        );
+      }
+      return Status.createNewStatus(lastStatus, 'Busy');
+    } catch (e) {
+      console.log(e.message);
     }
-    const { id } = material;
-    const statuses = await Status.find({
-      where: { material_id: id, person_id },
-    });
-    const lastStatus = statuses[statuses.length - 1];
-    if (lastStatus.status === 'Busy') {
-      return new BadRequestException(
-        `This book is busy or doesn't exist. Ask the manager.`
-      );
-    }
-    return Status.createNewStatus(lastStatus, 'Busy');
   }
 }
