e1af0b8ab132dd0eb58ff3005e98462f57699054
Ivan <82332668+IvanUglovets@users.noreply.github.com>
2022-04-14 15:51:28 +0300

    Feature/integrate UI kit in separate app (#56)
    * add ui-kit library
    
    * ready ui-kit integration
    
    * add new colors
    
    Co-authored-by: Ivan Uglovets <uglovec@gmil.com>

---
+++ packages/ui-kit/src/lib/theme/index.ts
@@ -0,0 +1,22 @@
+import { colors } from '../colors';
+
+export const theme = {
+  light: {
+    colors: {
+      primary_blue: colors.main_blue,
+    },
+    background: {
+      primary_bg: colors.bg_primary,
+    },
+  },
+  dark: {
+    colors: {
+      primary_blue: colors.main_blue,
+    },
+    background: {
+      primary_bg: colors.bg_primary,
+    },
+  },
+};
+
+//The same colors, because there is no theme change in the design yet. Then сan add different colors.
