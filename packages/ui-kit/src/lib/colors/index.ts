34e9e4817edbb08baa64b67f8c839f322bafa674
Ivan Uglovets <uglovec@gmail.com>
2022-04-14 12:22:18 +0300

    add new colors

--- packages/ui-kit/src/lib/colors/index.ts
+++ packages/ui-kit/src/lib/colors/index.ts
@@ -1,8 +1,9 @@
 export const colors = {
-  main_blue: '#1A1ED6',
+  accent_color: '#1A1ED6',
+  main_gray: '#828282',
   main_black: '#333333',
-  main_yellow: '#FF910F',
-  main_red: '#FF372A',
+  warning_yellow: '#FF910F',
+  problem_red: '#FF372A',
   main_green: '#2AA816',
   bg_primary: '#F3F7FF',
   bg_secondary: '#FFFFFF',
