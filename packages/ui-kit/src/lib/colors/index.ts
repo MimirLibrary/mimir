67342e935bdf16816daced8c8bda44cc23a02967
Ivan <82332668+IvanUglovets@users.noreply.github.com>
2022-04-15 11:42:30 +0300

    add new colors (#57)

--- packages/ui-kit/src/lib/colors/index.ts
+++ packages/ui-kit/src/lib/colors/index.ts
@@ -1,5 +1,8 @@
 export const colors = {
   accent_color: '#1A1ED6',
+  hover_color: '#14168F',
+  pressed_color: '#07097B',
+  gray_additional: '#E0E0E0',
   main_gray: '#828282',
   main_black: '#333333',
   warning_yellow: '#FF910F',
