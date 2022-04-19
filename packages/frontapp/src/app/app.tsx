9cdd622a01826a997fc5b7c1ea85fad8871c1fdb
Ivan Uglovets <uglovec@gmil.com>
2022-04-19 11:40:56 +0300

    remove unused components in app

--- packages/frontapp/src/app/app.tsx
+++ packages/frontapp/src/app/app.tsx
@@ -1,15 +1,5 @@
-import Search from './components/Search';
-import InstructionsClaim from './components/InstructionsClaim';
-import ListBooks from './components/ListBooks';
-
 export function App() {
-  return (
-    <>
-      <Search />
-      <InstructionsClaim />
-      <ListBooks />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
