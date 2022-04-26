41f9a319e9e6ae84f733aaef502a8140ab513165
Ivan Uglovets <uglovec@gmil.com>
2022-04-26 14:39:47 +0300

    test prettier v1

--- packages/frontapp/src/app/app.tsx
+++ packages/frontapp/src/app/app.tsx
@@ -29,7 +29,7 @@ const App: FC = () => {
   const { isAuth } = useAuth();
   return (
     <>
-      {isAuth ? (
+             {isAuth ? (
         <>
           <WrapperPage>
             <Sidebar />
