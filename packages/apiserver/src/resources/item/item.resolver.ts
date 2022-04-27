6d12aee314e07925d745ee72fa658373029d8120
Ivan <82332668+IvanUglovets@users.noreply.github.com>
2022-04-27 09:40:43 +0300

    fix bugs (#64)
    Co-authored-by: Ivan Uglovets <uglovec@gmil.com>

--- packages/apiserver/src/resources/item/item.resolver.ts
+++ packages/apiserver/src/resources/item/item.resolver.ts
@@ -1,20 +1,23 @@
-import {Args, Mutation, ResolveField, Resolver} from '@nestjs/graphql';
-import {ClaimBookInput, Error, Status, StatusResult} from "@mimir/global-types";
-import {Status as StatusType} from '../statuses/status.entity'
-import {ItemService} from "./item.service";
-import {ClaimError} from "../../errors";
+import { Args, Mutation, ResolveField, Resolver } from '@nestjs/graphql';
+import {
+  ClaimBookInput,
+  Error,
+  Status,
+  StatusResult,
+} from '@mimir/global-types';
+import { Status as StatusType } from '../statuses/status.entity';
+import { ItemService } from './item.service';
 
 @Resolver('StatusResult')
 export class ItemResolver {
-  constructor(private bookService: ItemService) {
-  }
+  constructor(private bookService: ItemService) {}
 
   @ResolveField()
   __resolveType(value) {
     if (value instanceof StatusType) {
       return 'Status';
     }
-    if (value instanceof ClaimError) {
+    if (value.message) {
       return 'Error';
     }
     return null;
@@ -22,12 +25,6 @@ export class ItemResolver {
 
   @Mutation()
   async claimBook(@Args('input') claimBookInput: ClaimBookInput) {
-    try {
-      return this.bookService.claim(claimBookInput)
-    } catch (e) {
-      return {
-        message: e.message
-      }
-    }
+    return this.bookService.claim(claimBookInput);
   }
 }
