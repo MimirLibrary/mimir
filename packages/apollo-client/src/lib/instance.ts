fedf747daf8f8dc4f3511b4722a143aaf75a2b43
x8er <x8er@yandex.com>
2022-06-16 17:37:13 +0300

    fix: nginx.conf & apollo-client

--- packages/apollo-client/src/lib/instance.ts
+++ packages/apollo-client/src/lib/instance.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: process.env['NX_API_ROOT_URL'] + '/graphql',
+  uri: 'http://localhost:3333/graphql',
   cache: new InMemoryCache(),
 });
