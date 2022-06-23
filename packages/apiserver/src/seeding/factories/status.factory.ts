c225e014035388fe18bf19124a849dae83c82506
Ivan Uglovets <uglovec@gmil.com>
2022-06-23 11:02:17 +0300

    fixing random fields

--- packages/apiserver/src/seeding/factories/status.factory.ts
+++ packages/apiserver/src/seeding/factories/status.factory.ts
@@ -2,14 +2,11 @@ import { define } from 'typeorm-seeding';
 import { Status } from '../../resources/statuses/status.entity';
 
 define(Status, () => {
-  const ids = [1, 2, 3, 4, 5];
+  const ids = [1, 2, 3];
   const getIdNumber = () => ids[Math.floor(Math.random() * ids.length)];
-  const statuses = ['Free', 'Busy'];
-  const getStatus = () => statuses[Math.floor(Math.random() * 2)];
-
   const status = new Status();
   status.material_id = getIdNumber();
   status.person_id = getIdNumber();
-  status.status = getStatus();
+  status.status = 'Free';
   return status;
 });
