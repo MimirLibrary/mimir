58cd5e32585748d2f2c06b16584bf0a9699dfcbd
Ivan Uglovets <uglovec@gmil.com>
2022-06-30 10:59:22 +0300

    fix mutation

--- packages/apollo-client/src/lib/generated/client-types.ts
+++ packages/apollo-client/src/lib/generated/client-types.ts
@@ -249,6 +249,7 @@ export type Query = {
   getOnePerson: Person;
   getStatusesByMaterial: Array<Maybe<Status>>;
   getStatusesByPerson: Array<Maybe<Status>>;
+  searchOfMaterials?: Maybe<Array<Maybe<Material>>>;
   welcome: Scalars['String'];
 };
 
@@ -287,6 +288,11 @@ export type QueryGetStatusesByPersonArgs = {
   person_id: Scalars['ID'];
 };
 
+
+export type QuerySearchOfMaterialsArgs = {
+  input: SearchInput;
+};
+
 export type RemoveLocationInput = {
   location_id: Scalars['Int'];
 };
@@ -300,6 +306,11 @@ export type RemoveMaterialInput = {
 export type RemoveNotificationInput = {
   material_id: Scalars['Int'];
   person_id: Scalars['Int'];
+};
+
+export type SearchInput = {
+  location: Scalars['String'];
+  search: Scalars['String'];
 };
 
 export type Status = {
@@ -343,6 +354,13 @@ export type CreateMessageForManagerMutationVariables = Exact<{
 
 export type CreateMessageForManagerMutation = { __typename?: 'Mutation', createMessageForManager: { __typename?: 'Error', message: string } | { __typename?: 'Message', message: string, title: string } };
 
+export type CreateNotificationMutationVariables = Exact<{
+  input: CreateNotificationInput;
+}>;
+
+
+export type CreateNotificationMutation = { __typename?: 'Mutation', createNotification?: { __typename?: 'Notification', id: string } | null };
+
 export type DonateBookMutationVariables = Exact<{
   person_id: Scalars['Int'];
   location_id: Scalars['Int'];
@@ -375,6 +393,13 @@ export type RemoveMaterialMutationVariables = Exact<{
 
 
 export type RemoveMaterialMutation = { __typename?: 'Mutation', removeMaterial: { __typename?: 'Material', identifier: string, id_type: string, type: string, location_id: number, title: string, author: string, category: string } };
+
+export type RemoveNotificationMutationVariables = Exact<{
+  input: RemoveNotificationInput;
+}>;
+
+
+export type RemoveNotificationMutation = { __typename?: 'Mutation', removeNotification?: { __typename?: 'Notification', id: string } | null };
 
 export type ReturnBookMutationVariables = Exact<{
   identifier: Scalars['String'];
@@ -522,6 +547,39 @@ export function useCreateMessageForManagerMutation(baseOptions?: Apollo.Mutation
 export type CreateMessageForManagerMutationHookResult = ReturnType<typeof useCreateMessageForManagerMutation>;
 export type CreateMessageForManagerMutationResult = Apollo.MutationResult<CreateMessageForManagerMutation>;
 export type CreateMessageForManagerMutationOptions = Apollo.BaseMutationOptions<CreateMessageForManagerMutation, CreateMessageForManagerMutationVariables>;
+export const CreateNotificationDocument = gql`
+    mutation CreateNotification($input: CreateNotificationInput!) {
+  createNotification(input: $input) {
+    id
+  }
+}
+    `;
+export type CreateNotificationMutationFn = Apollo.MutationFunction<CreateNotificationMutation, CreateNotificationMutationVariables>;
+
+/**
+ * __useCreateNotificationMutation__
+ *
+ * To run a mutation, you first call `useCreateNotificationMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useCreateNotificationMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [createNotificationMutation, { data, loading, error }] = useCreateNotificationMutation({
+ *   variables: {
+ *      input: // value for 'input'
+ *   },
+ * });
+ */
+export function useCreateNotificationMutation(baseOptions?: Apollo.MutationHookOptions<CreateNotificationMutation, CreateNotificationMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<CreateNotificationMutation, CreateNotificationMutationVariables>(CreateNotificationDocument, options);
+      }
+export type CreateNotificationMutationHookResult = ReturnType<typeof useCreateNotificationMutation>;
+export type CreateNotificationMutationResult = Apollo.MutationResult<CreateNotificationMutation>;
+export type CreateNotificationMutationOptions = Apollo.BaseMutationOptions<CreateNotificationMutation, CreateNotificationMutationVariables>;
 export const DonateBookDocument = gql`
     mutation DonateBook($person_id: Int!, $location_id: Int!, $identifier: String!, $id_type: String!, $type: String!, $title: String!, $author: String!, $category: String!, $description: String, $picture: String) {
   donateBook(
@@ -651,6 +709,39 @@ export function useRemoveMaterialMutation(baseOptions?: Apollo.MutationHookOptio
 export type RemoveMaterialMutationHookResult = ReturnType<typeof useRemoveMaterialMutation>;
 export type RemoveMaterialMutationResult = Apollo.MutationResult<RemoveMaterialMutation>;
 export type RemoveMaterialMutationOptions = Apollo.BaseMutationOptions<RemoveMaterialMutation, RemoveMaterialMutationVariables>;
+export const RemoveNotificationDocument = gql`
+    mutation RemoveNotification($input: RemoveNotificationInput!) {
+  removeNotification(input: $input) {
+    id
+  }
+}
+    `;
+export type RemoveNotificationMutationFn = Apollo.MutationFunction<RemoveNotificationMutation, RemoveNotificationMutationVariables>;
+
+/**
+ * __useRemoveNotificationMutation__
+ *
+ * To run a mutation, you first call `useRemoveNotificationMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useRemoveNotificationMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [removeNotificationMutation, { data, loading, error }] = useRemoveNotificationMutation({
+ *   variables: {
+ *      input: // value for 'input'
+ *   },
+ * });
+ */
+export function useRemoveNotificationMutation(baseOptions?: Apollo.MutationHookOptions<RemoveNotificationMutation, RemoveNotificationMutationVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useMutation<RemoveNotificationMutation, RemoveNotificationMutationVariables>(RemoveNotificationDocument, options);
+      }
+export type RemoveNotificationMutationHookResult = ReturnType<typeof useRemoveNotificationMutation>;
+export type RemoveNotificationMutationResult = Apollo.MutationResult<RemoveNotificationMutation>;
+export type RemoveNotificationMutationOptions = Apollo.BaseMutationOptions<RemoveNotificationMutation, RemoveNotificationMutationVariables>;
 export const ReturnBookDocument = gql`
     mutation ReturnBook($identifier: String!, $person_id: Int!) {
   returnItem(input: {identifier: $identifier, person_id: $person_id}) {
@@ -920,6 +1011,43 @@ export function useGetMaterialByIdLazyQuery(baseOptions?: Apollo.LazyQueryHookOp
 export type GetMaterialByIdQueryHookResult = ReturnType<typeof useGetMaterialByIdQuery>;
 export type GetMaterialByIdLazyQueryHookResult = ReturnType<typeof useGetMaterialByIdLazyQuery>;
 export type GetMaterialByIdQueryResult = Apollo.QueryResult<GetMaterialByIdQuery, GetMaterialByIdQueryVariables>;
+export const GetNotificationsByPersonDocument = gql`
+    query GetNotificationsByPerson($person_id: Int!) {
+  getNotificationsByPerson(person_id: $person_id) {
+    id
+    material_id
+    person_id
+  }
+}
+    `;
+
+/**
+ * __useGetNotificationsByPersonQuery__
+ *
+ * To run a query within a React component, call `useGetNotificationsByPersonQuery` and pass it any options that fit your needs.
+ * When your component renders, `useGetNotificationsByPersonQuery` returns an object from Apollo Client that contains loading, error, and data properties
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useGetNotificationsByPersonQuery({
+ *   variables: {
+ *      person_id: // value for 'person_id'
+ *   },
+ * });
+ */
+export function useGetNotificationsByPersonQuery(baseOptions: Apollo.QueryHookOptions<GetNotificationsByPersonQuery, GetNotificationsByPersonQueryVariables>) {
+        const options = {...defaultOptions, ...baseOptions}
+        return Apollo.useQuery<GetNotificationsByPersonQuery, GetNotificationsByPersonQueryVariables>(GetNotificationsByPersonDocument, options);
+      }
+export function useGetNotificationsByPersonLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetNotificationsByPersonQuery, GetNotificationsByPersonQueryVariables>) {
+          const options = {...defaultOptions, ...baseOptions}
+          return Apollo.useLazyQuery<GetNotificationsByPersonQuery, GetNotificationsByPersonQueryVariables>(GetNotificationsByPersonDocument, options);
+        }
+export type GetNotificationsByPersonQueryHookResult = ReturnType<typeof useGetNotificationsByPersonQuery>;
+export type GetNotificationsByPersonLazyQueryHookResult = ReturnType<typeof useGetNotificationsByPersonLazyQuery>;
+export type GetNotificationsByPersonQueryResult = Apollo.QueryResult<GetNotificationsByPersonQuery, GetNotificationsByPersonQueryVariables>;
 export const SearchOfMaterialsDocument = gql`
     query SearchOfMaterials($search: String!, $location: String!) {
   searchOfMaterials(input: {search: $search, location: $location}) {
@@ -966,40 +1094,3 @@ export function useSearchOfMaterialsLazyQuery(baseOptions?: Apollo.LazyQueryHook
 export type SearchOfMaterialsQueryHookResult = ReturnType<typeof useSearchOfMaterialsQuery>;
 export type SearchOfMaterialsLazyQueryHookResult = ReturnType<typeof useSearchOfMaterialsLazyQuery>;
 export type SearchOfMaterialsQueryResult = Apollo.QueryResult<SearchOfMaterialsQuery, SearchOfMaterialsQueryVariables>;
-export const GetNotificationsByPersonDocument = gql`
-    query GetNotificationsByPerson($person_id: Int!) {
-  getNotificationsByPerson(person_id: $person_id) {
-    id
-    material_id
-    person_id
-  }
-}
-    `;
-
-/**
- * __useGetNotificationsByPersonQuery__
- *
- * To run a query within a React component, call `useGetNotificationsByPersonQuery` and pass it any options that fit your needs.
- * When your component renders, `useGetNotificationsByPersonQuery` returns an object from Apollo Client that contains loading, error, and data properties
- * you can use to render your UI.
- *
- * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
- *
- * @example
- * const { data, loading, error } = useGetNotificationsByPersonQuery({
- *   variables: {
- *      person_id: // value for 'person_id'
- *   },
- * });
- */
-export function useGetNotificationsByPersonQuery(baseOptions: Apollo.QueryHookOptions<GetNotificationsByPersonQuery, GetNotificationsByPersonQueryVariables>) {
-        const options = {...defaultOptions, ...baseOptions}
-        return Apollo.useQuery<GetNotificationsByPersonQuery, GetNotificationsByPersonQueryVariables>(GetNotificationsByPersonDocument, options);
-      }
-export function useGetNotificationsByPersonLazyQuery(baseOptions?: Apollo.LazyQueryHookOptions<GetNotificationsByPersonQuery, GetNotificationsByPersonQueryVariables>) {
-          const options = {...defaultOptions, ...baseOptions}
-          return Apollo.useLazyQuery<GetNotificationsByPersonQuery, GetNotificationsByPersonQueryVariables>(GetNotificationsByPersonDocument, options);
-        }
-export type GetNotificationsByPersonQueryHookResult = ReturnType<typeof useGetNotificationsByPersonQuery>;
-export type GetNotificationsByPersonLazyQueryHookResult = ReturnType<typeof useGetNotificationsByPersonLazyQuery>;
-export type GetNotificationsByPersonQueryResult = Apollo.QueryResult<GetNotificationsByPersonQuery, GetNotificationsByPersonQueryVariables>;
