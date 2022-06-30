1d8eeb3a8785facd4b149a36c2b6388d2edbbd94
Ruslan Panasiuk <ruslan.panasiuk@itechart-group.com>
2022-06-30 15:22:26 +0500

    Apisrv: change DB name; squash migrations

--- packages/apiserver/src/migrations/1656584471709-migrationMy.ts
+++ packages/apiserver/src/migrations/1656584471709-migrationMy.ts
@@ -1,20 +1,33 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class migrationMy1655898874925 implements MigrationInterface {
-  name = 'migrationMy1655898874925';
+export class migrationMy1656584471709 implements MigrationInterface {
+  name = 'migrationMy1656584471709';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
+      `CREATE TABLE "status" ("id" SERIAL NOT NULL, "material_id" integer, "person_id" integer, "status" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_e12743a7086ec826733f54e1d95" PRIMARY KEY ("id"))`
+    );
+    await queryRunner.query(
       `CREATE TABLE "message" ("id" SERIAL NOT NULL, "material_id" integer, "person_id" integer, "title" character varying NOT NULL, "message" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_ba01f0a3e0123651915008bc578" PRIMARY KEY ("id"))`
+    );
+    await queryRunner.query(
+      `CREATE TABLE "person" ("id" SERIAL NOT NULL, "smg_id" character varying NOT NULL, "type" character varying NOT NULL, "location_id" integer, "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_5fdaf670315c4b7e70cce85daa3" PRIMARY KEY ("id"))`
     );
     await queryRunner.query(
       `CREATE TABLE "notification" ("id" SERIAL NOT NULL, "material_id" integer, "person_id" integer, "created_at" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "PK_705b6c7cdf9b2c2ff7ac7872cb7" PRIMARY KEY ("id"))`
     );
     await queryRunner.query(
+      `CREATE TABLE "material" ("id" SERIAL NOT NULL, "identifier" character varying NOT NULL, "id_type" character varying NOT NULL, "type" character varying NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT now(), "updated_at" TIMESTAMP NOT NULL DEFAULT now(), "picture" character varying, "title" character varying NOT NULL, "category" character varying NOT NULL, "author" character varying NOT NULL, "description" character varying NOT NULL, "location_id" integer, CONSTRAINT "PK_0343d0d577f3effc2054cbaca7f" PRIMARY KEY ("id"))`
+    );
+    await queryRunner.query(
       `CREATE TABLE "location" ("id" SERIAL NOT NULL, "location" character varying NOT NULL, CONSTRAINT "PK_876d7bdba03c72251ec4c2dc827" PRIMARY KEY ("id"))`
     );
-    await queryRunner.query(`ALTER TABLE "person" ADD "location_id" integer`);
-    await queryRunner.query(`ALTER TABLE "material" ADD "location_id" integer`);
+    await queryRunner.query(
+      `ALTER TABLE "status" ADD CONSTRAINT "FK_e58332a1b72ef781d05eece3a71" FOREIGN KEY ("material_id") REFERENCES "material"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "status" ADD CONSTRAINT "FK_dab4706ce6440c090ee89616d9e" FOREIGN KEY ("person_id") REFERENCES "person"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
+    );
     await queryRunner.query(
       `ALTER TABLE "message" ADD CONSTRAINT "FK_d945cd668d8f2b17602b348e8a5" FOREIGN KEY ("material_id") REFERENCES "material"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`
     );
@@ -54,10 +67,17 @@ export class migrationMy1655898874925 implements MigrationInterface {
     await queryRunner.query(
       `ALTER TABLE "message" DROP CONSTRAINT "FK_d945cd668d8f2b17602b348e8a5"`
     );
-    await queryRunner.query(`ALTER TABLE "material" DROP COLUMN "location_id"`);
-    await queryRunner.query(`ALTER TABLE "person" DROP COLUMN "location_id"`);
+    await queryRunner.query(
+      `ALTER TABLE "status" DROP CONSTRAINT "FK_dab4706ce6440c090ee89616d9e"`
+    );
+    await queryRunner.query(
+      `ALTER TABLE "status" DROP CONSTRAINT "FK_e58332a1b72ef781d05eece3a71"`
+    );
     await queryRunner.query(`DROP TABLE "location"`);
+    await queryRunner.query(`DROP TABLE "material"`);
     await queryRunner.query(`DROP TABLE "notification"`);
+    await queryRunner.query(`DROP TABLE "person"`);
     await queryRunner.query(`DROP TABLE "message"`);
+    await queryRunner.query(`DROP TABLE "status"`);
   }
 }
