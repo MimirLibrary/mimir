0e63dac304d7467bf8a7142d29f05ed6fd7db8c5
Ivan Uglovets <uglovec@gmil.com>
2022-07-29 10:43:29 +0300

    remove unused controller and imports

--- packages/metadata-service/src/readers/search.controller.ts
+++ packages/metadata-service/src/readers/search.controller.ts
@@ -1,12 +1,9 @@
 import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { ReaderService } from './reader.service';
-import { ChitaiGorodService } from './chitai-gorod.service';
 
 @Controller()
 export class SearchController {
-  constructor(
-    private readonly readerService: ReaderService // private readonly chitaiGorodService: ChitaiGorodService
-  ) {}
+  constructor(private readonly readerService: ReaderService) {}
 
   @Get('/search/:isbn')
   async search(@Param('isbn') isbn: string) {
@@ -17,14 +14,4 @@ export class SearchController {
     }
     return item;
   }
-
-  // @Get('/search1/:isbn')
-  // async search1(@Param('isbn') isbn: string) {
-  //   const item = await this.chitaiGorodService.readData(isbn);
-  //   if (item === undefined) {
-  //     console.error('Not Found!');
-  //     throw new NotFoundException(`Identifier "${isbn}" not found!`);
-  //   }
-  //   return item;
-  // }
 }
