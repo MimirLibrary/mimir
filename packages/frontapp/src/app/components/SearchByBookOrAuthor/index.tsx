d0117f69b7ebb0e70fe13f7ff3824b5a5ba2beed
Alexandra Danilevich <bloodangelalexandra@gmail.com>
2022-08-09 12:01:08 +0300

    Changes

--- packages/frontapp/src/app/components/SearchByBookOrAuthor/index.tsx
+++ packages/frontapp/src/app/components/SearchByBookOrAuthor/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import { t } from 'i18next';
 import { useSearchOfMaterialsQuery } from '@mimir/apollo-client';
 import { useNavigate } from 'react-router-dom';
@@ -6,25 +6,18 @@ import { useAppDispatch } from '../../hooks/useTypedDispatch';
 import { setActiveTab } from '../../store/slices/tabsSlice';
 import { setSearchMaterials } from '../../store/slices/materialsSlice';
 import { useDebounce } from '../../hooks/useDebounce';
-import { RoutesTypes } from '../../../utils/routes';
 import { useAppSelector } from '../../hooks/useTypedSelector';
 import Search from '../Search';
-import { RolesTypes } from '@mimir/global-types';
 
-const SearchByBookOrAuthor = () => {
+const SearchByBookOrAuthor: FC<{ path: string }> = ({ path }) => {
   const [search, setSearch] = useState<string>('');
-  const { location, userRole } = useAppSelector((state) => state.user);
+  const { location } = useAppSelector((state) => state.user);
   const debounceSearch = useDebounce<string>(search, 600);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
   const { data } = useSearchOfMaterialsQuery({
     variables: { search: debounceSearch, location: location?.value },
   });
-
-  const path =
-    userRole === RolesTypes.READER
-      ? `${RoutesTypes.SEARCH}_by_name_or_author`
-      : RoutesTypes.BOOKS_STUFF;
 
   useEffect(() => {
     if (data) {
