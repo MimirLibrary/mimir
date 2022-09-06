e98bb04d785dfd09e277b07e3ee203c29a96b6f2
Ivan <82332668+IvanUglovets@users.noreply.github.com>
2022-09-06 13:31:20 +0300

    add location dropdown on the ask manager form (#373)
    Co-authored-by: Ivan Uglovets <uglovec@gmil.com>

---
+++ packages/frontapp/src/app/components/ErrorBoundary/index.tsx
@@ -0,0 +1,43 @@
+import { Component, ErrorInfo, ReactNode } from 'react';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
+import { logout } from '../../store/slices/userSlice';
+import storage from 'redux-persist/lib/storage';
+import ErrorType500 from '../ErrorType500';
+
+interface Props {
+  children?: ReactNode;
+  dispatch: Dispatch;
+}
+
+interface IStateError {
+  error: boolean;
+}
+
+class ErrorBoundary extends Component<Props, IStateError> {
+  public override state: IStateError = {
+    error: false,
+  };
+
+  static getDerivedStateFromError(error: Error) {
+    return { error: true };
+  }
+
+  public override componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    storage.removeItem('persist:root');
+    this.props.dispatch(logout());
+    localStorage.clear();
+    location.reload();
+  }
+
+  public override render() {
+    if (this.state.error) {
+      return <ErrorType500 />;
+    }
+    return this.props.children;
+  }
+}
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({ dispatch });
+
+export default connect(null, mapDispatchToProps)(ErrorBoundary);
