34b4847c88a76f2c725290a64b62625dd9f1abfb
Ivan Uglovets <uglovec@gmail.com>
2022-09-09 15:04:47 +0300

    fix search of users

--- packages/apiserver/src/resources/persons/person.service.ts
+++ packages/apiserver/src/resources/persons/person.service.ts
@@ -7,19 +7,15 @@ import { RolesTypes } from '@mimir/global-types';
 export class PersonService {
   async getAllPersons(username: string, locations: Array<number>) {
     try {
-      const persons = await Person.createQueryBuilder('person')
+      return await Person.createQueryBuilder('person')
         .leftJoinAndSelect('person.location', 'location')
         .where(`${username ? 'person.username ILIKE :name' : ''}`, {
           name: `%${username}%`,
         })
         .andWhere('person.type = :type', { type: RolesTypes.READER })
+        .andWhere('location.id IN (:...locations)', { locations })
         .orderBy('person.username', 'ASC')
-        .addOrderBy('person.id', 'ASC')
         .getMany();
-      const filterPersons = persons.filter((person) =>
-        person.location.some((location) => locations.includes(location.id))
-      );
-      return filterPersons;
     } catch (e) {
       return new BadRequestException(e.message);
     }
