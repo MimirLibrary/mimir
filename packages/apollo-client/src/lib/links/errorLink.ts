4a4304117817ff4cc6c3644ab5e414f19951555d
sabrina34352 <s.babakulova@student.inha.uz>
2022-09-09 21:03:25 +0500

    requested fixes to UI

--- packages/apollo-client/src/lib/links/errorLink.ts
+++ packages/apollo-client/src/lib/links/errorLink.ts
@@ -1,12 +1,6 @@
 import { onError } from '@apollo/client/link/error';
 
 export const errorLink = onError(({ graphQLErrors }) => {
-  const userInfo = JSON.parse(localStorage.getItem('persist:root')!).user;
-  const userLocation = JSON.parse(userInfo).location;
-  if (graphQLErrors && !Array.isArray(userLocation)) {
-    localStorage.removeItem('persist:root');
-    window.location.reload();
-  }
   if (
     graphQLErrors &&
     graphQLErrors[0].message.startsWith('id-token is null')
