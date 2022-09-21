1dd2cad6fc5ce8b3d549ffa2f6bfe7df465f6da3
Ivan Uglovets <uglovec@gmil.com>
2022-09-21 14:15:31 +0300

    update migration

--- packages/apiserver/src/migrations/1663580664088-normalizeColumnISBN.ts
+++ packages/apiserver/src/migrations/1663580664088-normalizeColumnISBN.ts
@@ -4,22 +4,9 @@ import { Material } from '../resources/materials/material.entity';
 export class normalizeColumnISBN1663580664088 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     const materialRepo = await queryRunner.manager.getRepository(Material);
-    const materialsData = await materialRepo.query(
-      'SELECT id,identifier FROM material'
+    await materialRepo.query(
+      "UPDATE material SET identifier = REPLACE(identifier, '-', '')"
     );
-
-    if (!materialsData.length) return;
-
-    const updateMaterials = materialsData.map((item) => {
-      if (item.identifier.includes('-')) {
-        return materialRepo.update(item.id, {
-          identifier: item.identifier.replaceAll('-', ''),
-        });
-      }
-      return item;
-    });
-
-    await Promise.all(updateMaterials);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {}
