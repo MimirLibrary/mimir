c992f9f21d4620cd0563ba9ae6f4d6227e50830e
Chichmarin17 <pits1917burg@gmal.com>
2022-10-28 10:24:30 +0300

    feat: add dynamic statistics

--- packages/frontapp/src/app/components/StatisticsModal/index.tsx
+++ packages/frontapp/src/app/components/StatisticsModal/index.tsx
@@ -1,9 +1,14 @@
 import styled from '@emotion/styled';
 import { colors, dimensions } from '@mimir/ui-kit';
-import { FC, Dispatch, SetStateAction } from 'react';
+import { FC, Dispatch, SetStateAction, useEffect } from 'react';
 import Modal from '../Modal';
 import { t } from 'i18next';
 import Button from '../Button';
+import { useGetAllMaterialsForManagerQuery } from '@mimir/apollo-client';
+import { useAppSelector } from '../../hooks/useTypedSelector';
+import { locationIds } from '../../store/slices/userSlice';
+import { toast } from 'react-toastify';
+import { StatusTypes } from '@mimir/global-types';
 
 interface StatisticsModal {
   isActive: boolean;
@@ -34,7 +39,11 @@ const ButtonsWrapper = styled.div`
   gap: ${dimensions.base};
 `;
 
-const PieChart = styled.figure`
+const PieChart = styled.figure<{
+  free: number;
+  claimed: number;
+  overdue: number;
+}>`
   justify-self: center;
   background: radial-gradient(
       circle closest-side,
@@ -46,11 +55,12 @@ const PieChart = styled.figure`
     conic-gradient(
       from 270deg,
       ${colors.main_green} 0,
-      ${colors.main_green} 15%,
+      ${colors.main_green} ${(props) => props.free / 2}%,
       ${colors.accent_color} 0,
-      ${colors.accent_color} 45%,
+      ${colors.accent_color} ${(props) => props.free / 2 + props.claimed / 2}%,
       ${colors.problem_red} 0,
-      ${colors.problem_red} 50%,
+      ${colors.problem_red}
+        ${(props) => props.free / 2 + props.claimed / 2 + props.overdue / 2}%,
       ${colors.bg_secondary} 0,
       ${colors.bg_secondary} 100%
     );
@@ -58,6 +68,10 @@ const PieChart = styled.figure`
   height: 290px;
   border-radius: 50%;
   margin: 0;
+
+  @media (max-width: ${dimensions.phone_width}) {
+    margin-bottom: 2rem;
+  }
 `;
 
 const LegendWrapper = styled.div`
@@ -67,6 +81,12 @@ const LegendWrapper = styled.div`
   align-items: center;
   justify-content: space-between;
   gap: ${dimensions.xl_2};
+  @media (max-width: ${dimensions.phone_width}) {
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    gap: ${dimensions.xs_2};};
+  }
 `;
 
 const LegendItem = styled.div`
@@ -83,28 +103,78 @@ const LegendColor = styled.div`
 `;
 
 const StatisticsModal: FC<StatisticsModal> = ({ isActive, setIsActive }) => {
+  const locations = useAppSelector(locationIds);
+  const { data, error } = useGetAllMaterialsForManagerQuery({
+    variables: { locations },
+    fetchPolicy: 'no-cache',
+  });
+  const bookItems = data?.getAllMaterials!.map((material) => {
+    return { ...material, status: material?.statuses.slice(-1)[0] };
+  });
+
+  const NUMBER_OF_ALL_BOOKS =
+    bookItems?.filter(
+      (item) =>
+        item?.status?.status === StatusTypes.BUSY ||
+        item?.status?.status === StatusTypes.PROLONG ||
+        item?.status?.status === StatusTypes.FREE ||
+        item?.status?.status === StatusTypes.OVERDUE
+    ).length || 0;
+  const NUMBER_OF_FREE_BOOKS =
+    bookItems?.filter((item) => item?.status?.status === StatusTypes.FREE)
+      .length || 0;
+  const NUMBER_OF_CLAIMED_BOOKS =
+    bookItems?.filter(
+      (item) =>
+        item?.status?.status === StatusTypes.BUSY ||
+        item?.status?.status === StatusTypes.PROLONG
+    ).length || 0;
+  const NUMBER_OF_OVERDUE_BOOKS =
+    bookItems?.filter((item) => item?.status?.status === StatusTypes.OVERDUE)
+      .length || 0;
+
+  const STATISTICS = {
+    free: Math.round((NUMBER_OF_FREE_BOOKS / NUMBER_OF_ALL_BOOKS) * 100),
+    claimed: Math.round((NUMBER_OF_CLAIMED_BOOKS / NUMBER_OF_ALL_BOOKS) * 100),
+    overdue: Math.round((NUMBER_OF_OVERDUE_BOOKS / NUMBER_OF_ALL_BOOKS) * 100),
+  };
+
+  const closeModal = () => {
+    setIsActive(false);
+  };
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error.message);
+    }
+  }, [error]);
+
   return (
     <Modal active={isActive} setActive={setIsActive}>
       <Wrapper>
         <Title>{t('Statistics.Title')}</Title>
-        <PieChart></PieChart>
+        <PieChart
+          free={STATISTICS.free}
+          claimed={STATISTICS.claimed}
+          overdue={STATISTICS.overdue}
+        ></PieChart>
         <LegendWrapper>
           <LegendItem>
             <LegendColor color={colors.main_green}></LegendColor>
-            <span>On the shelf - 152</span>
+            <span>On the shelf - {NUMBER_OF_FREE_BOOKS}</span>
           </LegendItem>
           <LegendItem>
             <LegendColor color={colors.accent_color}></LegendColor>
-            <span>Claimed - 152</span>
+            <span>Claimed - {NUMBER_OF_CLAIMED_BOOKS}</span>
           </LegendItem>
           <LegendItem>
             <LegendColor color={colors.problem_red}></LegendColor>
-            <span>Overdue - 152</span>
+            <span>Overdue - {NUMBER_OF_OVERDUE_BOOKS}</span>
           </LegendItem>
         </LegendWrapper>
         <ButtonsWrapper>
-          <Button value="Ok" />
-          <Button transparent value="Cancel" />
+          <Button value="Ok" onClick={closeModal} />
+          <Button transparent value="Cancel" onClick={closeModal} />
         </ButtonsWrapper>
       </Wrapper>
     </Modal>
