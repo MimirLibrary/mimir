932d50361f071eed8f7139d6caaa18e67ab84256
Sabrina Babakulova <s.babakulova@student.inha.uz>
2022-11-08 15:35:11 +0500

    fix main page for manager (fix #448) (#468)
    * remove unnecesary button (fix #448)
    
    * redone main page for manager, with no duplicates of components
    
    * button back
    
    * add tests, and return button back
    
    * change title
    
    * update shapshots

--- packages/frontapp/src/app/components/ManagerInfoCard/index.tsx
+++ packages/frontapp/src/app/components/ManagerInfoCard/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useCallback, useState } from 'react';
 import styled from '@emotion/styled';
 import { ManagerCardTypes } from './managerCardTypes';
 import { colors, dimensions } from '@mimir/ui-kit';
@@ -9,11 +9,22 @@ import { IField, IOverdueItem } from '../../types';
 import { IMaterialDonate } from '../../types/donateList';
 import overdue_placeholder from '../../../assets/overdue_placeholder.png';
 import donate_placeholder from '../../../assets/donate_placeholder.png';
-
+import notification_placeholder from '../../../assets/notification_placeholder.png';
+import Modal from '../Modal';
+import AnswerToUser from '../AnswerToUser';
+import { ReactComponent as AddBookBluePlus } from '../../../assets/AddBookBluePlus.svg';
+import useScanner from '../../hooks/useScanner';
+import Scanner from '../Scanner';
 interface IManagerInfoCard {
   type: ManagerCardTypes;
   fieldsOverdue?: Array<IOverdueItem | null>;
   fieldsDonate?: Array<IMaterialDonate | null>;
+  fieldsNotification?: IField[] | null | undefined;
+}
+
+interface IDataOfMessage {
+  id: string;
+  person_id: string;
 }
 
 export const WrapperCard = styled.div`
@@ -25,11 +36,15 @@ export const WrapperCard = styled.div`
   width: 100%;
   box-shadow: ${colors.shadow};
   border-radius: ${dimensions.xs_1};
+  position: relative;
   padding: ${dimensions.xl_2} ${dimensions.xl_2} ${dimensions.xl_3}
     ${dimensions.xl_2};
   @media (max-width: ${dimensions.tablet_width}) {
+    box-shadow: none;
+    flex-direction: row;
     flex-wrap: wrap;
-    min-height: 467px;
+    min-height: 300px;
+    border-radius: 0;
     :first-of-type {
       margin-bottom: ${dimensions.xl_3};
     }
@@ -41,6 +56,9 @@ export const Title = styled.p`
   font-size: ${dimensions.xl_2};
   line-height: ${dimensions.xl_3};
   color: ${colors.main_black};
+  @media (max-width: ${dimensions.tablet_width}) {
+    font-size: ${dimensions.xl};
+  }
 `;
 
 export const FieldTitle = styled.p`
@@ -72,6 +90,9 @@ export const WrapperFooter = styled.div`
   flex-direction: row;
   bottom: ${dimensions.xs_1};
   justify-content: space-between;
+  @media (max-width: ${dimensions.tablet_width}) {
+    display: none;
+  }
 `;
 export const OpenLink = styled(Link)`
   cursor: pointer;
@@ -82,20 +103,6 @@ export const OpenLink = styled(Link)`
   width: 120px;
   text-align: right;
   text-decoration: underline;
-`;
-
-const InlineOpenLink = styled(OpenLink)`
-  flex: none;
-  position: absolute;
-  right: 71px;
-  order: 1;
-  flex-grow: 0;
-  text-align: center;
-  @media (max-width: ${dimensions.tablet_width}) {
-    position: static;
-    display: block;
-    text-align: center;
-  }
 `;
 
 export const FieldWrapper = styled.div`
@@ -118,6 +125,9 @@ export const FieldWrapper = styled.div`
 export const InlineWrapper = styled.div`
   display: flex;
   justify-content: space-between;
+  @media (max-width: ${dimensions.tablet_width}) {
+    display: block;
+  }
 `;
 
 interface IFieldOpenLinkProps {
@@ -165,10 +175,13 @@ const WrapperForEmptyBlock = styled.div`
   align-items: center;
   flex-direction: column;
   position: relative;
-
+  width: 100%;
   img {
     position: relative;
     z-index: 1;
+    @media (max-width: ${dimensions.tablet_width}) {
+      height: 100px;
+    }
   }
 `;
 
@@ -179,6 +192,10 @@ const WrapperCircle = styled.div`
   width: 100%;
   background-color: #f2f6ff;
   position: absolute;
+  @media (max-width: ${dimensions.tablet_width}) {
+    height: 170px;
+    max-width: 170px;
+  }
 `;
 
 const TitleEmpty = styled.h3<{ top: string }>`
@@ -190,6 +207,9 @@ const TitleEmpty = styled.h3<{ top: string }>`
   top: ${(props) => props.top};
   z-index: 1;
   text-align: center;
+  @media (max-width: ${dimensions.tablet_width}) {
+    display: none;
+  }
 `;
 
 const WrapperList = styled.div`
@@ -201,15 +221,82 @@ const WrapperList = styled.div`
   margin-top: ${dimensions.base};
 `;
 
+const InlineFieldDescription = styled(FieldDescription)`
+  max-width: 80%;
+  order: 0;
+  flex-grow: 0;
+`;
+
+const ButtonAnswer = styled.button`
+  flex: none;
+  outline: none;
+  border: none;
+  background: transparent;
+  text-decoration: underline;
+  color: ${colors.accent_color};
+  font-size: ${dimensions.base};
+  position: absolute;
+  right: 71px;
+  order: 1;
+  flex-grow: 0;
+  text-align: center;
+  cursor: pointer;
+  @media (max-width: ${dimensions.tablet_width}) {
+    position: static;
+    display: block;
+    text-align: center;
+  }
+`;
+const AddWrapper = styled.div`
+  display: none;
+  cursor: pointer;
+  @media (max-width: ${dimensions.tablet_width}) {
+    display: block;
+    position: absolute;
+    right: 20px;
+    top: 70px;
+  }
+`;
+
+const answers = [
+  'You have missed the due date for your book. Return it as soon as possible or contact the manager in room 35',
+  'We have accepted your donation to the library! Thank you!',
+  "If you don't check out all expired items in the library within a week, you will be banned from the app",
+];
+
 const ManagerInfoCard: FC<IManagerInfoCard> = ({
   type,
   fieldsOverdue,
   fieldsDonate,
+  fieldsNotification,
 }) => {
+  const [isAnswerModal, setIsAnswerModal] = useState<boolean>(false);
+  const [dataOfMessage, setDataOfMessage] = useState<IDataOfMessage | null>(
+    null
+  );
+  const handleAnswerModal = useCallback(
+    (dataOfMessage: IDataOfMessage) => {
+      setDataOfMessage(dataOfMessage);
+      setIsAnswerModal(true);
+    },
+    [dataOfMessage]
+  );
+  const {
+    isShowScanner,
+    setIsShowScanner,
+    handleOnDetectedScannerRoute,
+    handleOnCloseScanner,
+  } = useScanner();
+  const handleClose = useCallback(() => {
+    setIsAnswerModal(false);
+  }, []);
+  const handleClickOnOpenScanner = useCallback(() => {
+    setIsShowScanner(true);
+  }, [setIsShowScanner]);
   return (
     <>
       {fieldsOverdue && (
-        <WrapperCard>
+        <WrapperCard data-testid="overdueCard">
           <div>
             <Title>
               {t(`ManagerInfoCard.Title.${type}`) +
@@ -222,7 +309,11 @@ const ManagerInfoCard: FC<IManagerInfoCard> = ({
           {!fieldsOverdue.length ? (
             <WrapperForEmptyBlock>
               <WrapperCircle />
-              <img src={overdue_placeholder} alt="overdue_placeholder" />
+              <img
+                src={overdue_placeholder}
+                alt="overdue_placeholder"
+                data-testid="overdue-placeholder"
+              />
               <TitleEmpty top="45px">
                 {t('PlaceholderTitle.Overdue')}
               </TitleEmpty>
@@ -248,19 +339,18 @@ const ManagerInfoCard: FC<IManagerInfoCard> = ({
             </WrapperList>
           )}
           <WrapperFooter>
-            <>
-              <InlineWrapper>
-                {fieldsOverdue?.slice(0, 3).map((field) => (
-                  <StyledIcon key={field?.id} src={field?.person.avatar} />
-                ))}
-                {fieldsOverdue!.length > 3 ? (
-                  <NotificationDescription>
-                    {`+${fieldsOverdue.length - 3}` +
-                      t(`ManagerInfoCard.Description.More`)}
-                  </NotificationDescription>
-                ) : null}
-              </InlineWrapper>
-            </>
+            <InlineWrapper>
+              {fieldsOverdue?.slice(0, 3).map((field) => (
+                <StyledIcon key={field?.id} src={field?.person.avatar} />
+              ))}
+              {fieldsOverdue!.length > 3 ? (
+                <NotificationDescription>
+                  {`+${fieldsOverdue.length - 3}` +
+                    t(`ManagerInfoCard.Description.More`)}
+                </NotificationDescription>
+              ) : null}
+            </InlineWrapper>
+
             {!!fieldsOverdue.length && (
               <OpenLink to={`home/${type.toLowerCase()}`}>
                 {t(`ManagerInfoCard.Link.${type}`)}
@@ -270,7 +360,7 @@ const ManagerInfoCard: FC<IManagerInfoCard> = ({
         </WrapperCard>
       )}
       {fieldsDonate && (
-        <WrapperCard>
+        <WrapperCard data-testid="donateCard">
           <div>
             <Title>
               {t(`ManagerInfoCard.Title.${type}`) +
@@ -316,24 +406,23 @@ const ManagerInfoCard: FC<IManagerInfoCard> = ({
             </WrapperList>
           )}
           <WrapperFooter>
-            <>
-              <InlineWrapper>
-                {fieldsDonate?.slice(0, 3).map((field) => (
-                  <StyledIcon
-                    key={field?.id}
-                    src={
-                      field?.statuses[field?.statuses.length - 1]?.person.avatar
-                    }
-                  />
-                ))}
-                {fieldsDonate.length > 3 ? (
-                  <NotificationDescription>
-                    {`+${fieldsDonate.length - 3}` +
-                      t(`ManagerInfoCard.Description.More`)}
-                  </NotificationDescription>
-                ) : null}
-              </InlineWrapper>
-            </>
+            <InlineWrapper>
+              {fieldsDonate?.slice(0, 3).map((field) => (
+                <StyledIcon
+                  key={field?.id}
+                  src={
+                    field?.statuses[field?.statuses.length - 1]?.person.avatar
+                  }
+                />
+              ))}
+              {fieldsDonate.length > 3 ? (
+                <NotificationDescription>
+                  {`+${fieldsDonate.length - 3}` +
+                    t(`ManagerInfoCard.Description.More`)}
+                </NotificationDescription>
+              ) : null}
+            </InlineWrapper>
+
             {!!fieldsDonate.length && (
               <OpenLink to={`${RoutesTypes.DONATES_FROM_USER}`}>
                 {t(`ManagerInfoCard.Link.${type}`)}
@@ -342,6 +431,97 @@ const ManagerInfoCard: FC<IManagerInfoCard> = ({
           </WrapperFooter>
         </WrapperCard>
       )}
+      {fieldsNotification && (
+        <WrapperCard data-testid="notificationCard">
+          <div>
+            <Title>
+              {t(`ManagerInfoCard.Title.Notifications`) +
+                ` — (${fieldsNotification?.length})`}
+            </Title>
+            <AddWrapper onClick={handleClickOnOpenScanner}>
+              <AddBookBluePlus />
+            </AddWrapper>
+            <Description>
+              {t(`ManagerInfoCard.Description.Notifications`)}
+            </Description>
+          </div>
+          {!fieldsNotification.length ? (
+            <WrapperForEmptyBlock>
+              <WrapperCircle />
+              <img
+                src={notification_placeholder}
+                alt="notification_placeholder"
+                data-testid="notification-placeholder"
+              />
+              <TitleEmpty top="20px">
+                {t('PlaceholderTitle.Notifications')}
+              </TitleEmpty>
+            </WrapperForEmptyBlock>
+          ) : (
+            fieldsNotification?.slice(0, 3).map((field) => (
+              <FieldWrapper key={field.id} data-testid="newNotif">
+                <>
+                  <FieldTitle>{field.title}</FieldTitle>
+                  <InlineWrapper>
+                    <InlineFieldDescription>
+                      {field.message}
+                    </InlineFieldDescription>
+                    <ButtonAnswer
+                      onClick={() =>
+                        handleAnswerModal({
+                          id: field.id,
+                          person_id: field.person.id,
+                        })
+                      }
+                    >
+                      {t('ManagerInfoCard.Link.Answer')}
+                    </ButtonAnswer>
+                  </InlineWrapper>
+                  <FieldOpenLink
+                    to={`${RoutesTypes.READERS}/${field.person.id}`}
+                  >
+                    {field.person.username}
+                  </FieldOpenLink>
+                </>
+              </FieldWrapper>
+            ))
+          )}
+          <WrapperFooter>
+            <InlineWrapper>
+              {fieldsNotification?.slice(0, 3).map((field) => (
+                <StyledIcon key={field.id} src={field.person.avatar} />
+              ))}
+              {fieldsNotification!.length > 3 ? (
+                <NotificationDescription>
+                  {`+${fieldsNotification!.length - 3} ` +
+                    t(`ManagerInfoCard.Description.More`)}
+                </NotificationDescription>
+              ) : null}
+            </InlineWrapper>
+            {!!fieldsNotification.length && (
+              <OpenLink to="/notifications">
+                {t(`ManagerInfoCard.Link.Notifications`)}
+              </OpenLink>
+            )}
+          </WrapperFooter>
+        </WrapperCard>
+      )}
+      {isShowScanner && (
+        <Scanner
+          onDetected={handleOnDetectedScannerRoute}
+          onClose={handleOnCloseScanner}
+        />
+      )}
+      <Modal active={isAnswerModal} setActive={setIsAnswerModal}>
+        {isAnswerModal && (
+          <AnswerToUser
+            id={dataOfMessage?.id}
+            person_id={dataOfMessage?.person_id}
+            answers={answers}
+            close={handleClose}
+          />
+        )}
+      </Modal>
     </>
   );
 };
