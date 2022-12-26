3a47c8b9a084e5959225620f6266362547ad6b47
Arseni <yoinezero.a@gmail.com>
2022-12-26 20:02:58 +0300

    Fix: update data-transfer.service.ts
    - use fileService
    - extract transferFile() method from move() method

--- packages/apiserver/src/data-transfer/data-transfer.module.ts
+++ packages/apiserver/src/data-transfer/data-transfer.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DataTransferController } from './data-transfer.controller';
 import { DataTransferService } from './data-transfer.service';
 import { DigitalOceanModule } from '@mimir/api-util';
+import { FileModule } from '../file/file.module';
 
 @Module({
-  imports: [DigitalOceanModule],
+  imports: [DigitalOceanModule, FileModule],
   controllers: [DataTransferController],
   providers: [DataTransferService],
 })
--- packages/apiserver/src/data-transfer/data-transfer.service.ts
+++ packages/apiserver/src/data-transfer/data-transfer.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { Material } from '../resources/materials/material.entity';
 import { Like } from 'typeorm';
-import { deleteLocalFile, readLocalFile } from '@mimir/helper-functions';
 import { DigitalOceanService } from '@mimir/api-util';
 import * as process from 'process';
 import { DataTransferOut } from './data-transfer';
+import { FileService } from '../file/file.service';
 
 @Injectable()
 export class DataTransferService {
-  constructor(private digitalOceanService: DigitalOceanService) {}
+  constructor(
+    private digitalOceanService: DigitalOceanService,
+    private fileService: FileService
+  ) {}
 
   async move(): Promise<DataTransferOut> {
     const materials: Material[] = await Material.find({
@@ -21,16 +24,23 @@ export class DataTransferService {
     let errors = 0;
     for (const material of materials) {
       try {
-        const file = await readLocalFile(material.picture);
-        const url = (await this.digitalOceanService.createFile(file)) as string;
-        await Material.update(material.id, { picture: url });
-        await deleteLocalFile(material.picture);
+        await this.transferFile(material);
         successes++;
       } catch (e) {
-        console.error('ERROR:\n', e.message);
+        console.error(
+          'ERROR: An error occurred during file transfer.\nDetails: ',
+          e.message
+        );
         errors++;
       }
     }
     return { successes: successes, errors: errors };
+  }
+
+  private async transferFile(material: Material) {
+    const file = await this.fileService.readFile(material.picture);
+    const url = (await this.digitalOceanService.createFile(file)) as string;
+    await Material.update(material.id, { picture: url });
+    await this.fileService.removeFile(material.picture);
   }
 }
